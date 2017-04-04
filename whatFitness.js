783ceec1f2823b39166b0e21c89a3850fa53f6a6
Yoko  Ishizaki <yi35143@srcs.loc>
2017-04-04 14:12:10 -0700

    mistake

--- whatFitness.js
+++ whatFitness.js
@@ -1,3 +1,24 @@
 function randomExercise() {
+    var randomNum = Math.ceil(Math.random() * 3);
+    if (randomNum == 1) {
+        return "Do 30 jumping jacks";
+    } else if (randomNum == 2) {
+        return "Do a minute wall sit";
+    } else {
+        return "";
+    }
+}
+/*
+    var 1 = "Do 30 jumping jacks";
+    2 = "Do 10 push ups";
+    3 = "Do a 1-minute wall sit";
+    4 = "Jog in place for one minute";
+    5 = "Do the plank for one minute";
+    6 = "Do ten squats";
+    return Math.ceil((Math.random() * 6));
+} */
 
+function randomNumber() {
+    var x = inputId;
+    return Math.ceil(Math.random() * x);
 }
